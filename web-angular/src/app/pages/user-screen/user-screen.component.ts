0111002b8fa893964047ef3abd389289ef6fdd06
Felipe Sinnemann <felipe.a.malheiros.s@gmail.com>
2022-11-07 17:39:32 -0300

    Sistema de validação de inputs

--- web-angular/src/app/pages/user-screen/user-screen.component.ts
+++ web-angular/src/app/pages/user-screen/user-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailValidator } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,37 +12,106 @@ export class UserScreenComponent implements OnInit {
   constructor(
     private route: Router,
   ) { }
+  
 
   ngOnInit(): void {
   }
-  irMenu(){
+  goMenu(){
     this.route.navigate(['menu-page'])
   }
 
-  abrirLogin() {
+  openLogin() {
     let div = document.getElementById('loginForm');
     
-    this.fecharCadastro()
+    this.closeRegister()
     div!.style.display = "inline";
   }
 
-  abrirCadastro() {
-    let div = document.getElementById('cadastroForm');
+  openRegister() {
+    let div = document.getElementById('registerForm');
     
-    this.fecharLogin()
+    this.closeLogin()
 
     div!.style.display = "inline";
   }
 
-  fecharLogin() {
+  closeLogin() {
     let div = document.getElementById('loginForm');
     
     div!.style.display = "none";
   }
 
-  fecharCadastro() {
-    let div = document.getElementById('cadastroForm');
+  closeRegister() {
+    let div = document.getElementById('registerForm');
     
     div!.style.display = "none";
+  }
+
+  validateEmail(event: any) {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    let emailL = (<HTMLInputElement>document.getElementById("emailLoginTxt")).value
+    let emailR = (<HTMLInputElement>document.getElementById("emailRegisterTxt")).value
+
+    if (emailL !== "") {
+      if (emailRegex.test(emailL)) {
+        document.getElementById('emailLoginTxt')!.style.border = "";
+        document.getElementById('errorEmailLoginTxt')!.style.display = "";
+      } else {
+        document.getElementById('emailLoginTxt')!.style.border = "1px solid rgb(190, 12, 12)";
+        document.getElementById('errorEmailLoginTxt')!.style.display = "inline";
+      }
+    }
+    if (emailR !== "") {
+      if (emailRegex.test(emailR)) {
+        document.getElementById('emailRegisterTxt')!.style.border = "";
+        document.getElementById('errorEmailRegisterTxt')!.style.display = "";
+      } else {
+        document.getElementById('emailRegisterTxt')!.style.border = "1px solid rgb(190, 12, 12)";
+        document.getElementById('errorEmailRegisterTxt')!.style.display = "inline";
+      }
+    }
+  };
+  
+  validatePassword(event: any) {
+    const passwordRegex = /^[a-zA-Z0-9]{8,16}$/;
+    let passwordL = (<HTMLInputElement>document.getElementById('passwordLoginTxt')).value;
+    let passwordR = (<HTMLInputElement>document.getElementById('passwordRegisterTxt')).value;
+
+    if (passwordL !== "") {
+      if (passwordRegex.test(passwordL)) {
+        document.getElementById('passwordLoginTxt')!.style.border = "";
+        document.getElementById('errorPasswordLoginTxt')!.style.display = "";
+      } else {
+        document.getElementById('passwordLoginTxt')!.style.border = "1px solid rgb(190, 12, 12)";
+        document.getElementById('errorPasswordLoginTxt')!.style.display = "inline";
+      }
+    }
+    if (passwordR !== "") {
+      if (passwordRegex.test(passwordR)) {
+        document.getElementById('passwordRegisterTxt')!.style.border = "";
+        document.getElementById('errorPasswordRegisterTxt')!.style.display = "";
+      } else {
+        document.getElementById('passwordRegisterTxt')!.style.border = "1px solid rgb(190, 12, 12)";
+        document.getElementById('errorPasswordRegisterTxt')!.style.display = "inline";
+      }
+    }
+    
+  }
+
+  validateConfirmPassword(event: any) {
+    let password = (<HTMLInputElement>document.getElementById('passwordRegisterTxt')).value;
+    let passwordConfirm = (<HTMLInputElement>document.getElementById('passwordConfirmTxt')).value;
+
+    if (password === passwordConfirm || passwordConfirm == "") {
+      document.getElementById('passwordConfirmTxt')!.style.border = "";
+      document.getElementById('errorPasswordConfirmTxt')!.style.display = "";
+    } else {
+      document.getElementById('passwordConfirmTxt')!.style.border = "1px solid rgb(190, 12, 12)";
+      document.getElementById('errorPasswordConfirmTxt')!.style.display = "inline";
+    }
+  }
+
+  makeRegister() {
+    alert("Registrado")
   }
 }
